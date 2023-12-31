7daacd9771cd27f1592c325f270853b121a69e69
CustomLZH <2357316121@qq.com>
2023-12-31 16:56:44 +0800

    初始化

---
+++ src/main/java/com/monster/dyson/sphere/program/MonsterDysonSphereProgramApplication.java
@@ -0,0 +1,21 @@
+package com.monster.dyson.sphere.program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DSP启动类
+ *
+ * @author LiuZhaoHong
+ * @version 1.04
+ * @date 2023/12/3111
+ * @since JDK1.8
+ */
+@SpringBootApplication
+public class MonsterDysonSphereProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonsterDysonSphereProgramApplication.class, args);
+    }
+
+}
